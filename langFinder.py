3b2ca76ad27b2be1199fa6dcf338833aa2ccc318
LgAlexHub <alexlegras@hotmail.com>
2020-09-22 21:33:29 +0200

    Fini exo 1

--- langFinder.py
+++ langFinder.py
@@ -1,38 +1,46 @@
 import glob 
 import re 
 import json 
-
+print("===Importation du models.json===")
 jsonDictionnary = open("models.json","r")
 jsonContent = json.load(jsonDictionnary)
 def text_finder(path):
     file = open(path)
-    return file.read()
+    return file.read().decode("utf-8")
 
 
 def diagnostic_langue(text):
 
     dictionnary = {}
     chaine_split = text.split()
+    print("===Parsage du fichier d'apprentissage dans le dictionnaire===")
     for word in chaine_split:
         if word.lower() in dictionnary:
-            dictionnary[json.dumps(word.lower())] = dictionnary[json.dumps(word.lower())] + 1
+            dictionnary[word.lower()] = dictionnary[word.lower()] + 1
         else :
-            dictionnary[json.dumps(word.lower())] = 1
+            dictionnary[word.lower()] = 1
     dictionnary_sorted = {}
     dictionnary_sorted = sorted([[eff,mot] for mot, eff in dictionnary.items()],reverse=True)
-    print(dictionnary_sorted)
     verify_dictionnary = {}
-    for x in range(20):
+    #On regarde chaque mots du texte et one le compare avec les 10 mots les plus connu de notre model 
+    #si il est fait parti des mots les plus connu de notre model on ajoute un point dans la langue du mot du model 
+    #et on regarde quelle langue a le plus de point
+    for word in dictionnary_sorted:
         for lng in jsonContent:
-            if jsonContent[lng][x][1] in dictionnary_sorted[x][1]:
-                if lng in verify_dictionnary:
-                    verify_dictionnary[lng] = verify_dictionnary[lng] + 1
-                else : 
-                    verify_dictionnary[lng] = 1
+            for x in range(10):
+                if jsonContent[lng][x][1] == word[1]:
+                    if lng in verify_dictionnary:
+                        verify_dictionnary[lng] = verify_dictionnary[lng] + 1
+                    else:
+                        verify_dictionnary[lng] = 1
     verify_sorted = sorted([[eff,mot] for mot, eff in verify_dictionnary.items()],reverse=True)
+    #print(verify_dictionnary)
     return verify_sorted[0][1]
 
+      
+ 
 
 
-print(diagnostic_langue(text_finder("corpus_multi/fr/appr/2009-01-19_celex_IP-09-67.fr.html")))
-
+for path in glob.glob("*/it/appr/*"):
+       print(diagnostic_langue(text_finder(path)))
+#print(diagnostic_langue(text_finder("corpus_multi/en/appr/2009-01-14_celex_IP-09-48.en.html")))
