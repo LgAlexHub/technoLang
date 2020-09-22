e35bddf30a677508d403cad652313dfef61b8e7f
Alexis Legras <alexlegras@hotmail.com>
2020-09-22 16:41:57 +0200

    swapt branch

--- langFinder.py
+++ langFinder.py
@@ -6,30 +6,29 @@ jsonDictionnary = open("models.json","r")
 jsonContent = json.load(jsonDictionnary)
 def text_finder(path):
     file = open(path)
-    return file.read().encode('utf-8')
+    return file.read()
 
 
 def diagnostic_langue(text):
-    print(text)
+
     dictionnary = {}
     chaine_split = text.split()
     for word in chaine_split:
         if word.lower() in dictionnary:
-            dictionnary[word.lower()] = dictionnary[word.lower()] + 1
+            dictionnary[json.dumps(word.lower())] = dictionnary[json.dumps(word.lower())] + 1
         else :
-            dictionnary[word.lower()] = 1
+            dictionnary[json.dumps(word.lower())] = 1
     dictionnary_sorted = {}
     dictionnary_sorted = sorted([[eff,mot] for mot, eff in dictionnary.items()],reverse=True)
-    #print(dictionnary_sorted)
+    print(dictionnary_sorted)
     verify_dictionnary = {}
     for x in range(20):
         for lng in jsonContent:
-            for word in jsonContent[lng]:
-                if jsonContent[lng] in dictionnary_sorted[x][1]:
-                    if lng in verify_dictionnary:
-                        verify_dictionnary[lng] = verify_dictionnary[lng] + 1
-                    else : 
-                        verify_dictionnary[lng] = 1
+            if jsonContent[lng][x][1] in dictionnary_sorted[x][1]:
+                if lng in verify_dictionnary:
+                    verify_dictionnary[lng] = verify_dictionnary[lng] + 1
+                else : 
+                    verify_dictionnary[lng] = 1
     verify_sorted = sorted([[eff,mot] for mot, eff in verify_dictionnary.items()],reverse=True)
     return verify_sorted[0][1]
 
